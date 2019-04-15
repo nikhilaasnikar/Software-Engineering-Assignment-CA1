f87bc050440ee558fcc2d34dfecf0e63905f2ba5
Nikhil Aasnikar <nikhilasnikar@yahoo.com>
2019-04-15 14:29:27 +0100

    SortingComplete

--- WindowsFormsReadWrite/WindowsFormsReadWrite/Form1.cs
+++ WindowsFormsReadWrite/WindowsFormsReadWrite/Form1.cs
@@ -19,11 +19,11 @@ namespace WindowsFormsReadWrite
             selectfolder.RootFolder = Environment.SpecialFolder.Desktop;
         }
 
-       /* private void button1_Click(object sender, EventArgs e)
-        {
-            
+        /* private void button1_Click(object sender, EventArgs e)
+         {
 
-        }*/
+
+         }*/
 
         /*private void button3_Click(object sender, EventArgs e)
         {
@@ -35,6 +35,7 @@ namespace WindowsFormsReadWrite
             if (selectfolder.ShowDialog() == DialogResult.OK)
             {
                 List<string> dirlist = new List<string>();
+                List<string> calcArray = new List<string>();
                 List<string> folderList = new List<string>();
                 string path = selectfolder.SelectedPath;
                 string[] files = Directory.GetFiles(path);
@@ -43,15 +44,15 @@ namespace WindowsFormsReadWrite
                 {
                     if (Path.GetExtension(filename).Contains(".txt"))
                     {
-                        tbCurrent.Text = Path.GetFileName(filename)+"\n";
-                         
+                        tbCurrent.Text = Path.GetFileName(filename) + "\n";
+
                         StreamReader stream = new StreamReader(filename);
-                        
-                        string[] word =stream.ReadToEnd().Split(' ');
+
+                        string[] word = stream.ReadToEnd().Split(' ');
                         Array.Sort(word);
 
-                       string sortedfile = "\\sorted" + Path.GetFileName(filename);
-                       StreamWriter streamWriter = new StreamWriter(Path.GetFullPath(path)+sortedfile);
+                        string sortedfile = "\\sorted" + Path.GetFileName(filename);
+                        StreamWriter streamWriter = new StreamWriter(Path.GetFullPath(path) + sortedfile);
                         //Test//Console.WriteLine(Path.GetFullPath(path) + sortedfile);
                         // termsListà will be your list of sorted word
 
@@ -77,72 +78,78 @@ namespace WindowsFormsReadWrite
                             string itemKey = item.Key;
                             int itemValue = item.Value;
                             Console.WriteLine("Sorted values: " + itemKey + "," + itemValue);
-                            streamWriter.WriteLine(itemKey + "," + itemValue);
+                            if (itemValue == 1)
+                            {
+                                streamWriter.WriteLine(itemKey);
+
+                            }
+                            else
+                            {
+                                streamWriter.WriteLine(itemKey + "," + itemValue);
+                            }
+
                         }
                         streamWriter.Close();
-                           
+
                         dirlist.Add(Path.GetPathRoot(path));
                         folderList.Add(sortedfile);
                     }
+                    /*private void button2_Click(object sender, EventArgs e)
+                    {
+                        StreamReader sr = new StreamReader(tbCurrentDir.Text);
+                        tbCurrent.Text = sr.ReadToEnd();
+                        sr.Close();
+                    }
+
+                    private void button3_Click_1(object sender, EventArgs e)
+                    {
+                        StreamWriter sw = new StreamWriter(tbCurrentDir.Text, true);
+                        sw.WriteLine(textBox2.Text);
+                        sw.Close();
+                    }
+
+                    private void DriveList_SelectedIndexChanged(object sender, EventArgs e)
+                    {
+                        FolderList.Items.Clear();
+
+                        try
+                        {
+                            DriveInfo drive = (DriveInfo)DriveList.SelectedItem;
+                            foreach (DirectoryInfo dirInfo in drive.RootDirectory.GetDirectories())
+                                FolderList.Items.Add(dirInfo);
+
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+
+                    }
+
+                    private void FolderList_SelectedIndexChanged(object sender, EventArgs e)
+                    {
+                        FilesList.Items.Clear();
+
+                        DirectoryInfo dir = (DirectoryInfo)FolderList.SelectedItem;
+
+                        foreach (FileInfo fi in dir.GetFiles())
+                            FilesList.Items.Add(fi);
+
+                    }
+
+                    private void FilesList_SelectedIndexChanged(object sender, EventArgs e)
+                    {
+
+                    }
+
+                    private void Form1_Load(object sender, EventArgs e)
+                    {
+                        foreach (DriveInfo di in DriveInfo.GetDrives())
+                            DriveList.Items.Add(di);
+                    }
+                    */
                 }
-                DriveList.DataSource = dirlist;
-                FolderList.DataSource= folderList;
-         }
-
-        /*private void button2_Click(object sender, EventArgs e)
-        {
-            StreamReader sr = new StreamReader(tbCurrentDir.Text);
-            tbCurrent.Text = sr.ReadToEnd();
-            sr.Close();
-        }
-
-        private void button3_Click_1(object sender, EventArgs e)
-        {
-            StreamWriter sw = new StreamWriter(tbCurrentDir.Text, true);
-            sw.WriteLine(textBox2.Text);
-            sw.Close();
-        }
-
-        private void DriveList_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            FolderList.Items.Clear();
-
-            try
-            {
-                DriveInfo drive = (DriveInfo)DriveList.SelectedItem;
-                foreach (DirectoryInfo dirInfo in drive.RootDirectory.GetDirectories())
-                    FolderList.Items.Add(dirInfo);
-
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-
         }
-
-        private void FolderList_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            FilesList.Items.Clear();
-
-            DirectoryInfo dir = (DirectoryInfo)FolderList.SelectedItem;
-
-            foreach (FileInfo fi in dir.GetFiles())
-                FilesList.Items.Add(fi);
-
-        }
-
-        private void FilesList_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            foreach (DriveInfo di in DriveInfo.GetDrives())
-                DriveList.Items.Add(di);
-        }
-        */
-       }
     }
 }
